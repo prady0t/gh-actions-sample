169fa4c051bba6258e2a65c47501578d2216fe0f
Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>
2024-11-28 00:30:56 +0530

    checking for gh actions

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Welcome to the CI demo Flask app!"})
+    return jsonify({"message": "Welcome to the CI demo Flask app! "})
 
 @app.route("/health")
 def health():
