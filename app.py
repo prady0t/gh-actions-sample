0b24f3fb3d492d1b36b9ed995e3b034cfa946907
Pradyot Ranjan <99216956+pradyotRanjan@users.noreply.github.com>
2024-11-28 00:32:52 +0530

    correctiong gh failures

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return jsonify({"message": "Welcome to the CI demo Flask app! "})
+
 
 @app.route("/health")
 def health():
